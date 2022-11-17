2d6afa0bbc4e60ed6a12a2bc3927a07d82096b8b
Sobayo <sobayotoby@gmail.com>
2022-11-17 18:30:46 +0100

    improved the animations

--- Altschool-exam-project/src/components/Navbar/NavBar.jsx
+++ Altschool-exam-project/src/components/Navbar/NavBar.jsx
@@ -4,22 +4,24 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="logo">
-        <Link to="/">
-          Sobayo <span>Tobiloba</span>
-        </Link>
+      <div className="container">
+        <div className="logo">
+          <Link to="/">
+            Sobayo <span>Tobiloba</span>
+          </Link>
+        </div>
+        <nav>
+          <Link to="/" className="link">
+            Home
+          </Link>
+          <Link to="/about" className="link">
+            About
+          </Link>
+          <Link to="./Counter" className="link">
+            Counter
+          </Link>
+        </nav>
       </div>
-      <nav>
-        <Link to="/" className="link">
-          Home
-        </Link>
-        <Link to="/about" className="link">
-          About
-        </Link>
-        <Link to="./Counter" className="link">
-          Counter
-        </Link>
-      </nav>
     </div>
   );
 };
